b3f1a5c1c691e2566fab89136627122bf168996b
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2024-04-26 12:19:20 +0200

    Commit inicial.

--- src/main/java/org/iesalandalus/programacion/javafx/Main.java
+++ src/main/java/org/iesalandalus/programacion/javafx/Main.java
@@ -1,12 +1,10 @@
 package org.iesalandalus.programacion.javafx;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,24 +12,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage escenarioPrincipal) {
 		try {
-			HBox raiz = new HBox();
-			raiz.setPadding(new Insets(50, 50, 50, 50));
-			raiz.setAlignment(Pos.CENTER);
-			
-			Label lbRotada = new Label();
-			lbRotada.setText("Hola mundo desde JavaFX!!!!");
-			lbRotada.setFont(Font.font("Arial", 40));
-			lbRotada.setMinWidth(150);
-			
-			raiz.getChildren().add(lbRotada);
-			Scene escena = new Scene(raiz, 650, 150);
+			VBox raiz = FXMLLoader.load(getClass().getResource("/vistas/HolaMundo.fxml"));
+			Scene escena = new Scene(raiz);
+			escena.getStylesheets().add(getClass().getResource("/estilos/aplicacion.css").toExternalForm());
 			escenarioPrincipal.setTitle("Hola mundo desde JavaFX");
+			escenarioPrincipal.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/iconoSaludar.png")));
 			escenarioPrincipal.setScene(escena);
 			escenarioPrincipal.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 
 	public static void main(String[] args) {
 		launch(args);
