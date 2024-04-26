a82c93ca21b7844b9cb6f1db2a5e60df7d1f5e19
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2024-04-26 12:37:02 +0200

    Propago excepciones.

--- src/main/java/org/iesalandalus/programacion/javafx/Main.java
+++ src/main/java/org/iesalandalus/programacion/javafx/Main.java
@@ -7,23 +7,21 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main extends Application {
 
 	@Override
-	public void start(Stage escenarioPrincipal) {
-		try {
-			VBox raiz = FXMLLoader.load(getClass().getResource("/vistas/HolaMundo.fxml"));
-			Scene escena = new Scene(raiz);
-			escena.getStylesheets().add(getClass().getResource("/estilos/aplicacion.css").toExternalForm());
-			escenarioPrincipal.setTitle("Hola mundo desde JavaFX");
-			escenarioPrincipal.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/iconoSaludar.png")));
-			escenarioPrincipal.setScene(escena);
-			escenarioPrincipal.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage escenarioPrincipal) throws IOException {
+        VBox raiz = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vistas/HolaMundo.fxml")));
+        Scene escena = new Scene(raiz);
+		escena.getStylesheets().add(getClass().getResource("/estilos/aplicacion.css").toExternalForm());
+		escenarioPrincipal.setTitle("Hola mundo desde JavaFX");
+		escenarioPrincipal.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/iconoSaludar.png")));
+		escenarioPrincipal.setScene(escena);
+		escenarioPrincipal.show();
 	}
-
 
 	public static void main(String[] args) {
 		launch(args);
--- src/main/java/org/iesalandalus/programacion/javafx/controladores/HolaMundo.java
+++ src/main/java/org/iesalandalus/programacion/javafx/controladores/HolaMundo.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HolaMundo {
 
@@ -17,19 +18,15 @@ public class HolaMundo {
     private Label lbSaludo;
 
     @FXML
-    void saludar() {
-        try {
-            HBox raiz = FXMLLoader.load(getClass().getResource("/vistas/Saludo.fxml"));
-            Scene escena = new Scene(raiz);
-            escena.getStylesheets().add(getClass().getResource("/estilos/aplicacion.css").toExternalForm());
-            Stage escenarioSaludo = new Stage();
-            escenarioSaludo.setTitle("Saludo");
-            escenarioSaludo.setScene(escena);
-            escenarioSaludo.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/iconoSaludar.png")));
-            escenarioSaludo.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    void saludar() throws IOException {
+        HBox raiz = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/vistas/Saludo.fxml")));
+        Scene escena = new Scene(raiz);
+        escena.getStylesheets().add(getClass().getResource("/estilos/aplicacion.css").toExternalForm());
+        Stage escenarioSaludo = new Stage();
+        escenarioSaludo.setTitle("Saludo");
+        escenarioSaludo.setScene(escena);
+        escenarioSaludo.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/imagenes/iconoSaludar.png"))));
+        escenarioSaludo.show();
     }
 
 }
